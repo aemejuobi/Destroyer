866f95608a51cdf1777c262ef8ff6b3d240ba56c
aemejuobi <govallen@gmail.com>
2022-11-17 16:07:33 -0600

    folder with terraform main.tf

---
+++ delete/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "4.33.0"
+    }
+
+  }
+
+  provider {
+    region = "us-east-1"
+  }
+
+  resource "aws_connect_instance" "practice_instance" {
+    id = "0c1deacb-e3e4-4d18-817b-e57aaa3c0784"
+    # timeouts {
+    #     delete = "2m"
+    # }
+  }
+}
