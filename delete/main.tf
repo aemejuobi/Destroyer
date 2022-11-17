a5ac352d999e2a80e12ecdb3c267af95af8361c7
aemejuobi <govallen@gmail.com>
2022-11-17 17:00:06 -0600

    syntax edit to main.tf

--- delete/main.tf
+++ delete/main.tf
@@ -6,15 +6,15 @@ terraform {
     }
 
   }
+}
 
-  provider "aws" {
-    region = "us-east-1"
-  }
+provider "aws" {
+region = "us-east-1"
+}
 
-  resource "aws_connect_instance" "practice_instance" {
-    id = "0c1deacb-e3e4-4d18-817b-e57aaa3c0784"
-    # timeouts {
-    #     delete = "2m"
-    # }
-  }
+resource "aws_connect_instance" "practice_instance" {
+id = "0c1deacb-e3e4-4d18-817b-e57aaa3c0784"
+# timeouts {
+#     delete = "2m"
+# }
 }
