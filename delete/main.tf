04d13bd7488ae302bb4371dd51c1e18e1d5634f9
aemejuobi <govallen@gmail.com>
2022-11-17 17:11:13 -0600

    edited resource in main.tf

--- delete/main.tf
+++ delete/main.tf
@@ -9,11 +9,15 @@ terraform {
 }
 
 provider "aws" {
-region = "us-east-1"
+    region = "us-east-1"
 }
 
 resource "aws_connect_instance" "practice_instance" {
-id = "0c1deacb-e3e4-4d18-817b-e57aaa3c0784"
+    id = "0c1deacb-e3e4-4d18-817b-e57aaa3c0784"
+    identity_management_type = "CONNECT_MANAGED"
+    inbound_calls_enabled    = true
+    outbound_calls_enabled   = true
+    
 # timeouts {
 #     delete = "2m"
 # }
