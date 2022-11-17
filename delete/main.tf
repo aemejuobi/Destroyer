25ef8a59ce287d5105d89028c21bf23d47fbb989
aemejuobi <govallen@gmail.com>
2022-11-17 17:46:57 -0600

    edited resource in main.tf

--- delete/main.tf
+++ delete/main.tf
@@ -13,7 +13,6 @@ provider "aws" {
 }
 
 resource "aws_connect_instance" "practice_instance" {
-    id = "0c1deacb-e3e4-4d18-817b-e57aaa3c0784"
     identity_management_type = "CONNECT_MANAGED"
     inbound_calls_enabled    = true
     instance_alias           = "chocolate-connect"
